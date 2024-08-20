2f50d8350e1afc9c98a524e4aa409dfa9b2eb174
vinod <vinodpr737947@gmail.com>
2024-08-20 11:28:24 +0530

    day one

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ function submitUserData() {
   let NO_OF_FLOOR = document.querySelector("#floor").value;
   let NO_OF_LIFT = document.querySelector("#lift").value;
 
-  for (let i = 0; i < NO_OF_FLOOR; i++) {
+  for (let i=0; i<NO_OF_FLOOR; i++) {
+    
     const newFlat = document.createElement("div");
+    newFlat.setAttribute('id',`floor${i}`);
     if (i == 0) {
-      newFlat.innerHTML = `<div id='floor${i}'><button>DOWN</button> </div>`;
+      newFlat.innerHTML = `<button><img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/circled-chevron-down.png" alt="circled-chevron-down"/></button>`;
     } else if (i == NO_OF_FLOOR - 1) {
-      newFlat.innerHTML = `<div id='floor${i}'> <button>UP</button> </div>`;
+      newFlat.innerHTML = `<button><img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/circled-chevron-up.png" alt="circled-chevron-up"/></button>`;
     } else {
       newFlat.innerHTML =
-        `<div id='floor${i}'> <button>UP</button><button>DOWN</button> </div>`;
+        `<button><img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/circled-chevron-down.png" alt="circled-chevron-down"/></button><button><img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/circled-chevron-up.png" alt="circled-chevron-up"/></button>`;
     }
     const building = document.getElementById("newBuilding");
     building.appendChild(newFlat);
@@ -20,7 +22,8 @@ function submitUserData() {
 
   for(let i=0; i<NO_OF_LIFT; i++){
         const newLift=document.createElement('div');
-        newLift.innerHTML=`<div class='floor_lift'></div>`;
+        newLift.setAttribute('class','floor_lift')
+        newLift.setAttribute('id',`lift${i}`);
         groundFLoor.appendChild(newLift);
   }
 
